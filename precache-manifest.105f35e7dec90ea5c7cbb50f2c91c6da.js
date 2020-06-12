e1f4ab147b4d7ba0f921839e801bf7b97e9deb72
rsiritom <ricardo.a.siri@gmail.com>
2020-06-12 19:26:15 -0300

    Updates

---
+++ precache-manifest.105f35e7dec90ea5c7cbb50f2c91c6da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4a81dafca594d30a91db0aa219718a",
+    "url": "/anima/index.html"
+  },
+  {
+    "revision": "5cb0879d3a8a6e8bb6d0",
+    "url": "/anima/static/css/main.f8ef5267.chunk.css"
+  },
+  {
+    "revision": "5c11ec8cb99f3385a913",
+    "url": "/anima/static/js/2.f2857a81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anima/static/js/2.f2857a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb0879d3a8a6e8bb6d0",
+    "url": "/anima/static/js/main.b03e8c02.chunk.js"
+  },
+  {
+    "revision": "a3615dc07bdf867abd2f",
+    "url": "/anima/static/js/runtime-main.f3955173.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anima/static/media/logo.5d5d9eef.svg"
+  }
+]);
